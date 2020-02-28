6f0aac01c9a9d9771115343fd9255a7fef68db76
Equim <sayaka@ekyu.moe>
2020-02-29 01:19:25 +0800

    convlog: remove extra test

--- convlog/src/tests/mod.rs
+++ convlog/src/tests/mod.rs
@@ -1,7 +1,5 @@
 use crate::*;
 
-use serde_json;
-
 static TESTDATA: &[&str] = &[
     include_str!("testdata/chankan.json"),
     include_str!("testdata/four_reach.json"),
@@ -21,11 +19,4 @@ fn parse_and_convert() {
 
         assert!(!mjai_log.is_empty());
     });
-
-    let raw = r#"{"title":["",""],"name":["","","",""],"rule":{"aka":1},"log":[[[4,0,0],[25000,25000,25000,25000],[47],[],[12,14,51,16,22,24,31,53,36,37,16,18,44],[42,"c171618",47,33,46,32,47,47,19,45,21,27,22],[44,31,60,60,60,60,60,60,60,60,60,42],[11,11,11,11,11,11,11,11,11,11,11,11,11],[11,11,11,29,43,11,26,11,11,11,18,38],[41,44,29,60,60,12,60,46,15,31,60,60],[11,11,11,11,11,11,11,11,11,11,11,11,11],[11,41,11,32,31,11,16,11,11,13,11,17],[41,60,11,60,60,42,60,37,45,60,"r35",60],[11,11,11,11,11,11,11,11,11,11,11,11,11],[11,11,11,11,27,11,11,11,11,11,11,11],[17,43,44,28,60,38,32,35,31,41,11,11],["不明"]]]}"#;
-    let tenhou_log = tenhou::Log::from_json_string(raw).expect("failed to parse tenhou log");
-    let mjai_log = tenhou_to_mjai(&tenhou_log).expect("failed to transform tenhou log");
-    for log in &mjai_log {
-        println!("{}", serde_json::to_string(log).unwrap());
-    }
 }
