3c22f6677f0ad39afe34a0f7e2bca725da8e87a4
Equim <sayaka@ekyu.moe>
2020-03-03 00:13:48 +0800

    convlog: adjust imports

--- convlog/src/bin/main.rs
+++ convlog/src/bin/main.rs
@@ -1,12 +1,12 @@
+use convlog;
 use convlog::tenhou;
-use convlog::tenhou_to_mjai;
 use serde_json;
 use std::io;
 
 fn main() {
     let stdin = io::stdin();
     let tenhou_log = tenhou::Log::from_json_reader(stdin).expect("failed to parse tenhou log");
-    tenhou_to_mjai(&tenhou_log)
+    convlog::tenhou_to_mjai(&tenhou_log)
         .expect("failed to transform tenhou log")
         .iter()
         .for_each(|event| println!("{}", serde_json::to_string(event).unwrap()));
