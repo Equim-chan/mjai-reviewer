44f5cf5e97cb66cc0b54ccc51a0c4b989168751a
Equim <sayaka@ekyu.moe>
2020-03-03 22:24:05 +0800

    convlog: use macros to serialize into Pai array

--- convlog/src/mjai.rs
+++ convlog/src/mjai.rs
@@ -129,6 +129,15 @@ impl Event {
     }
 }
 
+macro_rules! make_pai_array {
+    ($array:ident, $($index:expr),*) => {
+        [$(Pai::from(&*$array[$index])),*]
+    };
+    ($matrix:ident[$first_index:expr], $($index:expr),*) => {
+        [$(Pai::from(&*$matrix[$first_index][$index])),*]
+    };
+}
+
 #[inline]
 fn deserialize_pai_from_str<'de, D>(deserializer: D) -> Result<Pai, D::Error>
 where
@@ -137,100 +146,43 @@ where
     Ok(Pai::from(&*String::deserialize(deserializer)?))
 }
 
+#[inline]
 fn deserialize_pai_2_from_str<'de, D>(deserializer: D) -> Result<[Pai; 2], D::Error>
 where
     D: Deserializer<'de>,
 {
     let s = <[String; 2]>::deserialize(deserializer)?;
-    Ok([Pai::from(&*s[0]), Pai::from(&*s[1])])
+    Ok(make_pai_array!(s, 0, 1))
 }
 
+#[inline]
 fn deserialize_pai_3_from_str<'de, D>(deserializer: D) -> Result<[Pai; 3], D::Error>
 where
     D: Deserializer<'de>,
 {
     let s = <[String; 3]>::deserialize(deserializer)?;
-    Ok([Pai::from(&*s[0]), Pai::from(&*s[1]), Pai::from(&*s[2])])
+    Ok(make_pai_array!(s, 0, 1, 2))
 }
 
+#[inline]
 fn deserialize_pai_4_from_str<'de, D>(deserializer: D) -> Result<[Pai; 4], D::Error>
 where
     D: Deserializer<'de>,
 {
     let s = <[String; 4]>::deserialize(deserializer)?;
-    Ok([
-        Pai::from(&*s[0]),
-        Pai::from(&*s[1]),
-        Pai::from(&*s[2]),
-        Pai::from(&*s[3]),
-    ])
+    Ok(make_pai_array!(s, 0, 1, 2, 3))
 }
 
+#[inline]
 fn deserialize_tehais_from_str<'de, D>(deserializer: D) -> Result<[[Pai; 13]; 4], D::Error>
 where
     D: Deserializer<'de>,
 {
     let s = <[[String; 13]; 4]>::deserialize(deserializer)?;
     Ok([
-        [
-            Pai::from(&*s[0][0]),
-            Pai::from(&*s[0][1]),
-            Pai::from(&*s[0][2]),
-            Pai::from(&*s[0][3]),
-            Pai::from(&*s[0][4]),
-            Pai::from(&*s[0][5]),
-            Pai::from(&*s[0][6]),
-            Pai::from(&*s[0][7]),
-            Pai::from(&*s[0][8]),
-            Pai::from(&*s[0][9]),
-            Pai::from(&*s[0][10]),
-            Pai::from(&*s[0][11]),
-            Pai::from(&*s[0][12]),
-        ],
-        [
-            Pai::from(&*s[1][0]),
-            Pai::from(&*s[1][1]),
-            Pai::from(&*s[1][2]),
-            Pai::from(&*s[1][3]),
-            Pai::from(&*s[1][4]),
-            Pai::from(&*s[1][5]),
-            Pai::from(&*s[1][6]),
-            Pai::from(&*s[1][7]),
-            Pai::from(&*s[1][8]),
-            Pai::from(&*s[1][9]),
-            Pai::from(&*s[1][10]),
-            Pai::from(&*s[1][11]),
-            Pai::from(&*s[1][12]),
-        ],
-        [
-            Pai::from(&*s[2][0]),
-            Pai::from(&*s[2][1]),
-            Pai::from(&*s[2][2]),
-            Pai::from(&*s[2][3]),
-            Pai::from(&*s[2][4]),
-            Pai::from(&*s[2][5]),
-            Pai::from(&*s[2][6]),
-            Pai::from(&*s[2][7]),
-            Pai::from(&*s[2][8]),
-            Pai::from(&*s[2][9]),
-            Pai::from(&*s[2][10]),
-            Pai::from(&*s[2][11]),
-            Pai::from(&*s[2][12]),
-        ],
-        [
-            Pai::from(&*s[3][0]),
-            Pai::from(&*s[3][1]),
-            Pai::from(&*s[3][2]),
-            Pai::from(&*s[3][3]),
-            Pai::from(&*s[3][4]),
-            Pai::from(&*s[3][5]),
-            Pai::from(&*s[3][6]),
-            Pai::from(&*s[3][7]),
-            Pai::from(&*s[3][8]),
-            Pai::from(&*s[3][9]),
-            Pai::from(&*s[3][10]),
-            Pai::from(&*s[3][11]),
-            Pai::from(&*s[3][12]),
-        ],
+        make_pai_array!(s[0], 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+        make_pai_array!(s[1], 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+        make_pai_array!(s[2], 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+        make_pai_array!(s[3], 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
     ])
 }
