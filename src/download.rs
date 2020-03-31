a04c1d5f0e92529e0ab29eb8840c5cf7b3687491
Equim <sayaka@ekyu.moe>
2020-03-31 22:25:08 +0800

    download: support proxy

--- src/download.rs
+++ src/download.rs
@@ -1,15 +1,25 @@
 use std::io::prelude::*;
 
 use anyhow::anyhow;
-use anyhow::Result;
+use anyhow::{Context, Result};
+use env_proxy;
 use ureq;
 
 pub fn download_tenhou_log(log_id: &str) -> Result<impl Read> {
-    let res = ureq::get(&format!("https://tenhou.net/5/mjlog2json.cgi?{}", log_id))
-        .set("Referer", &format!("https://tenhou.net/6/?log={}", log_id))
-        .timeout_connect(10_000)
-        .call();
+    let url = format!("https://tenhou.net/5/mjlog2json.cgi?{}", log_id);
+    let referer = format!("https://tenhou.net/6/?log={}", log_id);
 
+    let mut req = ureq::get(&url);
+    req.set("Referer", &referer);
+    req.timeout_connect(10_000);
+
+    if let Some(proxy_url) = env_proxy::for_url_str(&url).raw_value() {
+        let proxy_str: String = proxy_url.chars().skip("http://".len()).collect();
+        let proxy = ureq::Proxy::new(proxy_str).context("failed to parse proxy")?;
+        req.set_proxy(proxy);
+    }
+
+    let res = req.call();
     if res.ok() {
         Ok(res.into_reader())
     } else {
