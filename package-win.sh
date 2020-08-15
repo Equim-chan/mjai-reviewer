bc05de775f36fe87c5664fad81f7fde2e2d5e13d
Equim <sayaka@ekyu.moe>
2020-08-16 03:08:54 +0800

    add package script for windows

---
+++ package-win.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# NOTE: This script is not intended for general uses.
+
+echo 'NOTE: make sure to have removed `-march=native` and `-C target-cpu=native`.'
+set -ex
+
+AKOCHAN_DIR=$HOME/akochan
+REVIEWER_DIR=$HOME/akochan-reviewer
+OUT_DIR="$(mktemp -d)"
+
+pushd "$AKOCHAN_DIR"
+pushd ai_src
+make -j$(nproc)
+popd
+make -j$(nproc)
+mkdir -p "$OUT_DIR"/akochan
+cp -rt "$OUT_DIR"/akochan \
+    system.exe \
+    ai.dll \
+    params \
+    LICENSE \
+    /mingw64/bin/libgcc_s_seh-1.dll \
+    /mingw64/bin/libgomp-1.dll \
+    /mingw64/bin/libstdc++-6.dll \
+    /mingw64/bin/libwinpthread-1.dll
+popd
+
+pushd "$REVIEWER_DIR"
+cargo build --release
+cp -rt "$OUT_DIR" \
+    target/release/akochan-reviewer.exe \
+    tactics.json \
+    LICENSE
+popd
+
+7z a -tzip akochan.zip "$OUT_DIR"/*
+rm -rf "$OUT_DIR"
