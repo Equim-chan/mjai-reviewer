a0adbf3d7d0aad741268564dcacace1f72090dae
Equim <sayaka@ekyu.moe>
2023-01-28 11:48:55 +0800

    add data-mark-red

--- templates/report.js
+++ templates/report.js
@@ -3,44 +3,38 @@ function toggleLayout() {
   if (layoutVertical) {
     document.body.removeAttribute('horizontal')
     const lboxes = document.querySelectorAll('.l-box')
-    for (let i = 0; i < lboxes.length; i++) {
-      lboxes[i].removeAttribute('horizontal')
+    for (const box of lboxes) {
+      box.removeAttribute('horizontal')
     }
     const rboxes = document.querySelectorAll('.r-box')
-    for (let i = 0; i < rboxes.length; i++) {
-      rboxes[i].removeAttribute('horizontal')
+    for (const box of rboxes) {
+      box.removeAttribute('horizontal')
     }
   } else {
     document.body.setAttribute('horizontal', '1')
     const lboxes = document.querySelectorAll('.l-box')
-    for (let i = 0; i < lboxes.length; i++) {
-      lboxes[i].setAttribute('horizontal', '1')
+    for (const box of lboxes) {
+      box.setAttribute('horizontal', '1')
     }
     const rboxes = document.querySelectorAll('.r-box')
-    for (let i = 0; i < rboxes.length; i++) {
-      rboxes[i].setAttribute('horizontal', '1')
+    for (const box of rboxes) {
+      box.setAttribute('horizontal', '1')
     }
   }
 }
 
 function toggleDiffOnly() {
   const diffOnly = document.querySelector('#diff-only-yes').checked
-  const turnList = document.querySelectorAll('details.collapse')
-  for (const turn of turnList) {
-    const turnInfo = turn.querySelector('.turn-info')
-    if (turnInfo) {
-      if (diffOnly && !turnInfo.querySelector('.order-loss')) {
-        turn.removeAttribute('open')
-      } else {
-        turn.setAttribute('open', '')
-      }
+  const entries = document.querySelectorAll('details.collapse.entry')
+  for (const entry of entries) {
+    if (diffOnly && !entry.hasAttribute('data-mark-red')) {
+      entry.removeAttribute('open')
+    } else {
+      entry.setAttribute('open', '')
     }
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const elems = document.querySelectorAll('.latex')
-  for (const elem of elems) {
-    renderMathInElement(elem)
-  }
+  document.querySelectorAll('.latex').forEach(renderMathInElement)
 })
