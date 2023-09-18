6b516f40b1df679db8653ffe557228042313f06e
Equim <sayaka@ekyu.moe>
2023-09-18 20:47:23 +0800

    add back-to-top button, fix #113

--- templates/report.js
+++ templates/report.js
@@ -1,46 +1,63 @@
 function toggleLayout() {
-  const layout = document.querySelector('#panel input[name="layout"]:checked').value
+  const layout = document.querySelector('#panel input[name="layout"]:checked').value;
   if (layout === 'vertical') {
-    document.body.removeAttribute('data-horizontal')
-    const lboxes = document.querySelectorAll('.l-box')
+    document.body.removeAttribute('data-horizontal');
+    const lboxes = document.querySelectorAll('.l-box');
     for (const box of lboxes) {
-      box.removeAttribute('data-horizontal')
+      box.removeAttribute('data-horizontal');
     }
-    const rboxes = document.querySelectorAll('.r-box')
+    const rboxes = document.querySelectorAll('.r-box');
     for (const box of rboxes) {
-      box.removeAttribute('data-horizontal')
+      box.removeAttribute('data-horizontal');
     }
   } else {
-    document.body.setAttribute('data-horizontal', '')
-    const lboxes = document.querySelectorAll('.l-box')
+    document.body.setAttribute('data-horizontal', '');
+    const lboxes = document.querySelectorAll('.l-box');
     for (const box of lboxes) {
-      box.setAttribute('data-horizontal', '')
+      box.setAttribute('data-horizontal', '');
     }
-    const rboxes = document.querySelectorAll('.r-box')
+    const rboxes = document.querySelectorAll('.r-box');
     for (const box of rboxes) {
-      box.setAttribute('data-horizontal', '')
+      box.setAttribute('data-horizontal', '');
     }
   }
 }
 
 function toggleExpand() {
-  const expand = document.querySelector('#panel input[name="expand"]:checked').value
-  const entries = document.querySelectorAll('details.collapse.entry')
+  const expand = document.querySelector('#panel input[name="expand"]:checked').value;
+  const entries = document.querySelectorAll('details.collapse.entry');
   for (const entry of entries) {
     switch (expand) {
       case 'all':
-        entry.open = true
-        break
+        entry.open = true;
+        break;
       case 'diff-only':
-        entry.open = entry.hasAttribute('data-mark-red')
-        break
+        entry.open = entry.hasAttribute('data-mark-red');
+        break;
       case 'none':
-        entry.open = false
-        break
+        entry.open = false;
+        break;
     }
   }
 }
 
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function scrollHandler() {
+  const topButton = document.querySelector('#top-button');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.classList.add('show');
+  } else {
+    topButton.classList.remove('show');
+  }
+}
+
+document.addEventListener('scroll', scrollHandler);
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.latex').forEach(renderMathInElement)
-})
+  scrollHandler();
+  document.querySelectorAll('.latex').forEach(renderMathInElement);
+});
